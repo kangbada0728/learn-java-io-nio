95ddc8dbc4600635ec61bb705b3da6dd2764c6e5
bada.kang <jarring.helper0x@icloud.com>
2023-10-09 09:37:47 +0900

    add: NormalThreadTest

---
+++ src/main/java/me/kangbada/thread/NormalThreadTest.java
@@ -0,0 +1,25 @@
+package me.kangbada.thread;
+
+public class NormalThreadTest {
+    public static void main(String[] args) {
+        // 스레드를 생성한다.
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    // 5초 간 멈춘다.
+                    Thread.sleep(5000);
+                    // 스레드 종료 메시지
+                    System.out.println("MyThread 종료");
+                } catch (Exception e) {
+                    // 무시한다.
+                }
+            }
+        };
+        // 스레드를 시작한다.
+        t.start();
+
+        // main 메소드 종료 메시지
+        System.out.println("main() 종료");
+    }
+}
