23afa3d19687f344b127a2cf0c6f82a7590e8e95
bada.kang <jarring.helper0x@icloud.com>
2023-10-14 21:13:52 +0900

    add: SystemInputTest

---
+++ src/main/java/me/kangbada/stream/SystemInputTest.java
@@ -0,0 +1,17 @@
+package me.kangbada.stream;
+
+import java.io.IOException;
+
+public class SystemInputTest {
+    public static void main(String[] args) {
+        int i = 0;
+
+        try {
+            while ((i = System.in.read()) != -1) {
+                System.out.write(i);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
